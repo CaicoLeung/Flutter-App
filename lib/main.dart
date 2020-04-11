26793bc3d17af96b8f47c02a9fcce70cbebd48c6
CaicoLeung <lkk1053190902@gmail.com>
2020-04-11 16:28:34 +0800

    雏形

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'text_widget_page.dart' show TextPage;
@@ -15,13 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '至尊宝物',
       initialRoute: 'home',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        primaryColor: Colors.white
       ),
       routes: {
-        'home': (context) => MyHomePage(title: '聚友财', color: Color.fromRGBO(251, 219, 76, 1)),
+        'home': (context) => MyHomePage(),
         'text': (context) => TextPage(),
         'button': (context) => ButtonPage(),
         'image': (context) => ImagePage(),
@@ -37,25 +39,45 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.color}) : super(key: key);
-
-  final String title;
-  final Color color;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+Widget setTabbarItem(String path) {
+  return Container(
+    width: 32.0,
+    height: 32.0,
+    child: Image.asset(path),
+  );
+}
+
+
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   TabController _tabController;
   List tabs = ['新闻', '历史', '图片'];
   int _selectedIndex = 0;
+  String words = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  static const loadingTag = "##loading##";
+  var _words = <String>[loadingTag];
+
+  void _retrieveData() {
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      setState(() {
+        _words: _words.insertAll(_words.length - 1, generateWordPairs().take(20).map((f) => f.asPascalCase).toList());
+      });
+    });
+  }
 
   @override
   void initState() {
     print('initState');
     super.initState();
-    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController = TabController(initialIndex: 0, length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      print('当前Tabs index: ${_tabController.index}');
+    });
   }
 
   @override
@@ -92,83 +114,133 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {},
-            )
-          ],
+          title: Text('至尊宝物'),
           bottom: TabBar(
             controller: _tabController,
-            tabs: tabs.map((e) => Tab(text: e,)).toList(),
+            labelColor: Colors.black,
+            tabs: tabs
+                .map((e) => Tab(
+                      text: e,
+                    ))
+                .toList(),
           ),
-          backgroundColor: widget.color,
-          centerTitle: false,
+          backgroundColor: Colors.white,
           textTheme: TextTheme(title: TextStyle(color: Color.fromRGBO(51, 51, 51, 1), fontSize: 20, fontWeight: FontWeight.w600)),
         ),
         drawer: new MyDrawer(),
         bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          iconSize: 22.0,
+          selectedItemColor: Color.fromRGBO(255, 51, 95, 1),
+          unselectedItemColor: Color.fromRGBO(74, 74, 74, 1),
           items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
-            BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/tabbar/faxian.png'), activeIcon: Image.asset('assets/tabbar/faxian-select.png'), title: Text('首页')),
+            BottomNavigationBarItem(icon: setTabbarItem('assets/tabbar/release.png'), title: Text('')),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/tabbar/wode-not.png'), activeIcon: Image.asset('assets/tabbar/wode.png'), title: Text('我的')),
           ],
           currentIndex: _selectedIndex,
-          fixedColor: Colors.blue,
           onTap: _onItemTapped,
         ),
-        body: Center(
-          child: DefaultTextStyle(
-            style: TextStyle(color: Colors.blueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton.icon(
-                  color: Colors.yellow,
-                  icon: Icon(Icons.short_text),
-                  label: Text('Process'),
-                  onPressed: () => Navigator.pushNamed(context, 'progress'),
-                ),
-                FlatButton.icon(
-                  color: Colors.cyan,
-                  icon: Icon(Icons.inbox),
-                  label: Text('ConstrainedBox'),
-                  onPressed: () => Navigator.pushNamed(context, 'constrained'),
-                ),
-                FlatButton.icon(
-                  color: Colors.deepPurpleAccent,
-                  icon: Icon(Icons.receipt),
-                  label: Text('layout'),
-                  onPressed: () => Navigator.pushNamed(context, 'layout'),
-                ),
-                FlatButton(
-                  child: Icon(Icons.input),
-                  onPressed: () => Navigator.pushNamed(context, 'textfield'),
-                ),
-                RaisedButton.icon(
-                    onPressed: () => Navigator.pushNamed(context, 'switch'),
-                    color: Colors.teal,
-                    icon: Icon(
-                      Icons.pan_tool,
-                      color: Colors.white,
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Center(
+              child: DefaultTextStyle(
+                style: TextStyle(color: Colors.blueAccent),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton.icon(
+                      color: Colors.yellow,
+                      icon: Icon(Icons.short_text),
+                      label: Text('Process'),
+                      onPressed: () => Navigator.pushNamed(context, 'progress'),
                     ),
-                    label: Text(
-                      'Switch_Widget',
-                      style: TextStyle(color: Colors.white),
-                    )),
-                RaisedButton(
-                  child: Text('Button_Widget'),
-                  onPressed: () => Navigator.pushNamed(context, 'button'),
+                    FlatButton.icon(
+                      color: Colors.cyan,
+                      icon: Icon(Icons.inbox),
+                      label: Text('ConstrainedBox'),
+                      onPressed: () => Navigator.pushNamed(context, 'constrained'),
+                    ),
+                    FlatButton.icon(
+                      color: Colors.deepPurpleAccent,
+                      icon: Icon(Icons.receipt),
+                      label: Text('layout'),
+                      onPressed: () => Navigator.pushNamed(context, 'layout'),
+                    ),
+                    FlatButton(
+                      child: Icon(Icons.input),
+                      onPressed: () => Navigator.pushNamed(context, 'textfield'),
+                    ),
+                    RaisedButton.icon(
+                        onPressed: () => Navigator.pushNamed(context, 'switch'),
+                        color: Colors.teal,
+                        icon: Icon(
+                          Icons.pan_tool,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Switch_Widget',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    RaisedButton(
+                      child: Text('Button_Widget'),
+                      onPressed: () => Navigator.pushNamed(context, 'button'),
+                    ),
+                    FlatButton(
+                      child: Text('Text_Widget'),
+                      onPressed: () => Navigator.pushNamed(context, 'text'),
+                    ),
+                    OutlineButton.icon(onPressed: () => Navigator.pushNamed(context, 'image'), icon: Icon(Icons.image), label: Text('Image_Widget'))
+                  ],
                 ),
-                FlatButton(
-                  child: Text('Text_Widget'),
-                  onPressed: () => Navigator.pushNamed(context, 'text'),
-                ),
-                OutlineButton.icon(onPressed: () => Navigator.pushNamed(context, 'image'), icon: Icon(Icons.image), label: Text('Image_Widget'))
-              ],
+              ),
             ),
-          ),
+            SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: words.split('').map((f) => Text(f, style: TextStyle(height: 2),)).toList(),
+                ),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                ListTile(title: Text('今天要背的单词: ', style: TextStyle(decoration: TextDecoration.underline),),),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: _words.length,
+                    separatorBuilder: (context, index) => Divider(height: 1.0,),
+                    itemBuilder: (context, index) {
+                      if(_words[index] == loadingTag) {
+                        if(_words.length < 50) {
+                          _retrieveData();
+                          return Container(
+                            padding: EdgeInsets.all(16.0),
+                            alignment: Alignment.center,
+                            child: SizedBox(
+                              width: 24.0,
+                              height: 24.0,
+                              child: CircularProgressIndicator(strokeWidth: 2.0,),
+                            ),
+                          );
+                        } else {
+                          return Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.all(16.0),
+                            child: Text('没有更多了', style: TextStyle(color: Colors.grey),),
+                          );
+                        }
+                      }
+                      return ListTile(title: Text(_words[index]),);
+                    },
+                  ),
+                )
+              ],
+            )
+          ],
         ));
   }
 
@@ -178,13 +250,12 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     });
   }
 
-  void _onAdd() {
-
-  }
+  void _onAdd() {}
 }
 
 class MyDrawer extends StatelessWidget {
-  const MyDrawer({ Key key }): super(key: key);
+  const MyDrawer({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
